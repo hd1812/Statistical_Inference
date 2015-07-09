03421efa9cfc393d93bf5c3db4dff13e4381e360
hd1812 <hao.ding12@imperial.ac.uk>
2015-07-09 23:14:54 +0100

    Week 1 Intro

---
+++ Intro.R
@@ -0,0 +1,32 @@
+####Week 1 Introduction
+
+##Useful concepts
+##PMF,PDF -- discrete and contineous
+##cpf, quantile, survival -- complimentary
+
+
+##Code Sample
+##pbeta(0.75,2,1) --0.75 quantile with triangle height 2, width 1
+
+##Probability model connects data to population using assumptions
+##Most statistical terms are estimands
+
+####Conditional Probability
+##Sentivity +D   Specivicity -Dc  prevalance (P(D))(popularity)
+
+##Likelihood ratio
+##P(D|+)/P(Dc|+) => P(+|D)/P(+|Dc) * P(D)/P(Dc)
+##Post-test odds=> DLR+ * Pre-test odds
+##DLR (Diagnostic Likelihood ratio)
+
+##Independence
+##P(A^B)=P(A)P(B) only when AB are independent
+
+##Expected values
+##Popultaion mean is estimated by sample mean
+##sample code: manimupate
+
+##Sample average itself is random variable, more centerred around population mean
+##Expected value of sample is same as population mean if estimator is un  biased
+
+
